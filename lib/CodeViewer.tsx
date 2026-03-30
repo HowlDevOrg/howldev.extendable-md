21c276d7c52a0b45c905cf85d6f02a350b9e6650
Cody Howell <codyhowell44@gmail.com>
2026-03-30 08:55:17 -0600

    converted to functional component

--- lib/CodeViewer.tsx
+++ lib/CodeViewer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import typescript from 'highlight.js/lib/languages/typescript';
@@ -23,73 +23,52 @@ hljs.registerLanguage('sql', sql);
 /** 
  * Code Viewer takes in a language and an array of strings with the backtick(``) preferred for their definition. They use 
  * HighlightJS to do syntax highlighting for JS, TS, C#, XML, and YML. 
- * @example <CodeViewer langauge="YAML/C#" codeLines={[
-           `// Program.cs`,
-           `reader["second"]["arrayOfObjects"][0]["lorem"].AsString(); // "ipsum"`,
-           `reader["first"]["simple Array"][2].AsInt();                // 3`
-         ]} />
  */
-export class CodeViewer extends React.Component<{ langauge: string, codeLines: string }, { buttonColor: string }> {
-  state = {
-    buttonColor: "#242424"
-  }
+export function CodeViewer(props: { langauge: string, codeLines: string }) {
+  const [buttonColor, setButtonColor] = useState("#242424");
+  const codeRef = useRef<HTMLElement | null>(null);
 
-  private codeRef = React.createRef<HTMLElement>();
-
-  componentDidMount(): void {
-    this.highlightCode();
-  }
-
-  componentDidUpdate(prevProps: { codeLines: string }) {
-    if (prevProps.codeLines !== this.props.codeLines) {
-      this.highlightCode();
+  useEffect(() => {
+    if (codeRef.current) {
+      // Unset existing highlight state before applying a new one.
+      codeRef.current.removeAttribute("data-highlighted");
+      hljs.highlightElement(codeRef.current);
     }
-  }
+  }, [props.codeLines]);
 
-  highlightCode(): void {
-    if (this.codeRef.current) {
-      // Unset existing highlight state before applying a new one
-      this.codeRef.current.removeAttribute("data-highlighted");
-
-      // Now apply highlighting
-      hljs.highlightElement(this.codeRef.current);
-    }
-  }
-
-  copyToClipboard = (): void => {
-    navigator.clipboard.writeText(this.props.codeLines).then(
+  const copyToClipboard = (): void => {
+    navigator.clipboard.writeText(props.codeLines).then(
       () => console.log("Text copied to clipboard!"),
       (err) => console.error("Failed to copy text: ", err)
     );
-    this.setState({ buttonColor: "#1c8c12" });
-    setTimeout(() => this.setState({ buttonColor: "#242424" }), 3000);
-  }
 
+    setButtonColor("#1c8c12");
+    setTimeout(() => setButtonColor("#242424"), 3000);
+  };
 
-  render() {
-    const buttonTextColor = this.state.buttonColor === "#1c8c12" ? "#000000" : "#ffffff";
-    return (
-      <div className="code-interface">
-        <div>
-          <p>{this.props.langauge}</p>
-          <button
-            onClick={this.copyToClipboard}
-            style={{
-              alignSelf: "end",
-              backgroundColor: `${this.state.buttonColor}`,
-              color: `${buttonTextColor}`,
-              borderRadius: "10px",
-              cursor: "pointer"
-            }}
-          >Copy</button>
-        </div>
-        <pre>
-          <code ref={this.codeRef} className={'language-' + this.props.langauge}>
-            {this.props.codeLines}
-          </code>
-        </pre>
+  const buttonTextColor = buttonColor === "#1c8c12" ? "#000000" : "#ffffff";
+
+  return (
+    <div className="code-interface">
+      <div>
+        <p>{props.langauge}</p>
+        <button
+          onClick={copyToClipboard}
+          style={{
+            alignSelf: "end",
+            backgroundColor: `${buttonColor}`,
+            color: `${buttonTextColor}`,
+            borderRadius: "10px",
+            cursor: "pointer"
+          }}
+        >Copy</button>
       </div>
-    );
-  }
+      <pre>
+        <code ref={codeRef} className={'language-' + props.langauge}>
+          {props.codeLines}
+        </code>
+      </pre>
+    </div>
+  );
 }
 
