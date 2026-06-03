cec255219f552f3133663085356b03b4a1e9916d
Cody Howell <codyhowell44@gmail.com>
2026-06-02 22:29:03 -0600

    another test refactor

--- testing/interactive/execute.returns.functions.test.ts
+++ testing/interactive/execute.returns.functions.test.ts
@@ -5,131 +5,106 @@ import { ParamDef } from "../../lib/logic/Interactive/types";
 describe("code can run simple functions with prims", () => {
   const paramDefs: ParamDef[] = [];
   const values: string[] = [];
-  it("e()", () => {
-    const code = ["return e()"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value.slice(0, 4)).toBe("2.71");
-  });
-  it("pi()", () => {
-    const code = ["return pi()"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value.slice(0, 4)).toBe("3.14");
-  });
-  it("sqrt()", () => {
-    const code = ["return sqrt(16)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("4");
-  });
-  it("sin()", () => {
-    const code = ["return sin(0)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("0");
-  });
-  it("cos()", () => {
-    const code = ["return cos(0)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("1");
-  });
-  it("tan()", () => {
-    const code = ["return tan(0)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("0");
-  });
-  it("round(1.5)", () => {
-    const code = ["return round(1.5)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("2");
-  });
-  it("round(200.1)", () => {
-    const code = ["return round(200.1)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("200");
-  });
-  it("floor(1.5)", () => {
-    const code = ["return floor(1.5)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("1");
-  });
-  it("floor(200.1)", () => {
-    const code = ["return floor(200.1)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("200");
-  });
-  it("ceil(1.5)", () => {
-    const code = ["return ceil(1.5)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("2");
-  });
-  it("ceil(200.1)", () => {
-    const code = ["return ceil(200.1)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("201");
-  });
-  it("degtorad(180)", () => {
-    const code = ["return degtorad(180)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value.slice(0, 4)).toBe("3.14");
-  });
-  it("radtodeg(2.0943951024)", () => {
-    const code = ["return radtodeg(2.0943951024)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value.slice(0, 5)).toBe("120.0");
-  });
-  it("log(100)", () => {
-    const code = ["return log(100)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("2");
-  });
-  it("log2(64)", () => {
-    const code = ["return log2(64)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("6");
-  });
-  it("ln(20)", () => {
-    const code = ["return ln(20)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe(Math.log(20).toString());
-  });
-  it("abs(-120)", () => {
-    const code = ["return abs(-120)"];
-    const modified = ExecuteCode(paramDefs, values, code);
-    expect(modified.length).toBe(1);
-    expect(modified[0].label).toBe("");
-    expect(modified[0].value).toBe("120");
+  const functions = [
+    {
+      name: "e()",
+      code: "return e()",
+      check: (val: string) => expect(val.slice(0, 4)).toBe("2.71"),
+    },
+    {
+      name: "pi()",
+      code: "return pi()",
+      check: (val: string) => expect(val.slice(0, 4)).toBe("3.14"),
+    },
+    {
+      name: "sqrt()",
+      code: "return sqrt(16)",
+      check: (val: string) => expect(val).toBe("4"),
+    },
+    {
+      name: "sin()",
+      code: "return sin(0)",
+      check: (val: string) => expect(val).toBe("0"),
+    },
+    {
+      name: "cos()",
+      code: "return cos(0)",
+      check: (val: string) => expect(val).toBe("1"),
+    },
+    {
+      name: "tan()",
+      code: "return tan(0)",
+      check: (val: string) => expect(val).toBe("0"),
+    },
+    {
+      name: "round(1.5)",
+      code: "return round(1.5)",
+      check: (val: string) => expect(val).toBe("2"),
+    },
+    {
+      name: "round(200.1)",
+      code: "return round(200.1)",
+      check: (val: string) => expect(val).toBe("200"),
+    },
+    {
+      name: "floor(1.5)",
+      code: "return floor(1.5)",
+      check: (val: string) => expect(val).toBe("1"),
+    },
+    {
+      name: "floor(200.1)",
+      code: "return floor(200.1)",
+      check: (val: string) => expect(val).toBe("200"),
+    },
+    {
+      name: "ceil(1.5)",
+      code: "return ceil(1.5)",
+      check: (val: string) => expect(val).toBe("2"),
+    },
+    {
+      name: "ceil(200.1)",
+      code: "return ceil(200.1)",
+      check: (val: string) => expect(val).toBe("201"),
+    },
+    {
+      name: "degtorad(180)",
+      code: "return degtorad(180)",
+      check: (val: string) => expect(val.slice(0, 4)).toBe("3.14"),
+    },
+    {
+      name: "radtodeg(2.0943951024)",
+      code: "return radtodeg(2.0943951024)",
+      check: (val: string) => expect(val.slice(0, 5)).toBe("120.0"),
+    },
+    {
+      name: "log(100)",
+      code: "return log(100)",
+      check: (val: string) => expect(val).toBe("2"),
+    },
+    {
+      name: "log2(64)",
+      code: "return log2(64)",
+      check: (val: string) => expect(val).toBe("6"),
+    },
+    {
+      name: "ln(20)",
+      code: "return ln(20)",
+      check: (val: string) => expect(val).toBe(Math.log(20).toString()),
+    },
+    {
+      name: "abs(-120)",
+      code: "return abs(-120)",
+      check: (val: string) => expect(val).toBe("120"),
+    },
+  ];
+  functions.forEach((func) => {
+    it(func.name, () => {
+      const code = [func.code];
+      const modified = ExecuteCode(paramDefs, values, code);
+      expect(modified.length).toBe(1);
+      expect(modified[0].label).toBe("");
+      func.check(modified[0].value);
+    });
   });
 });
 
