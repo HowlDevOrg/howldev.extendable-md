2c01f676d9cbf02100f973187e38197cc946950d
Cody Howell <codyhowell44@gmail.com>
2026-06-05 22:21:38 -0600

    second level test cases

--- testing/interactive/execute.switch.test.ts
+++ testing/interactive/execute.switch.test.ts
@@ -3,9 +3,9 @@ import { ExecuteCode } from "../../lib/logic/Calculator/ExecuteCode";
 import { ParamDef } from "../../lib/logic/Calculator/types";
 
 describe("code can run switch statements with literals (1 level)", () => {
+  const paramDefs: ParamDef[] = [];
+  const values: string[] = [];
   it("switch with string and inner return (true)", () => {
-    const paramDefs: ParamDef[] = [];
-    const values: string[] = [];
     const code = ['switch "this"', 'case "this"', "return true", "endswitch"];
     const modified = ExecuteCode(paramDefs, values, code);
     expect(modified.length).toBe(1);
@@ -13,16 +13,12 @@ describe("code can run switch statements with literals (1 level)", () => {
     expect(modified[0].value).toBe("true");
   });
   it("switch with string and inner return (false)", () => {
-    const paramDefs: ParamDef[] = [];
-    const values: string[] = [];
     const code = ['switch "this"', 'case "that"', "return true", "endswitch"];
     expect(() => ExecuteCode(paramDefs, values, code)).toThrowError(
       "Did not find a return statement.",
     );
   });
   it("switch with number and inner return (true)", () => {
-    const paramDefs: ParamDef[] = [];
-    const values: string[] = [];
     const code = ["switch 15", "case 15", "return true", "endswitch"];
     const modified = ExecuteCode(paramDefs, values, code);
     expect(modified.length).toBe(1);
@@ -30,12 +26,73 @@ describe("code can run switch statements with literals (1 level)", () => {
     expect(modified[0].value).toBe("true");
   });
   it("switch with number and inner return (false)", () => {
-    const paramDefs: ParamDef[] = [];
-    const values: string[] = [];
     const code = ["switch 15", "case 20", "return true", "endswitch"];
     expect(() => ExecuteCode(paramDefs, values, code)).toThrowError(
       "Did not find a return statement.",
     );
+  });
+});
+
+describe("code can run switch statements with literals (2 levels)", () => {
+  const paramDefs: ParamDef[] = [];
+  const values: string[] = [];
+  it("switch with string and inner return (first)", () => {
+    const code = [
+      'switch "this"',
+      'case "this"',
+      "return true",
+      `case "that"`,
+      "return false",
+      "endswitch",
+    ];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(1);
+    expect(modified[0].label).toBe("");
+    expect(modified[0].value).toBe("true");
+  });
+  it("switch with string and inner return (second)", () => {
+    const code = [
+      'switch "that"',
+      'case "this"',
+      "return true",
+      `case "that"`,
+      "return false",
+      "endswitch",
+    ];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(1);
+    expect(modified[0].label).toBe("");
+    expect(modified[0].value).toBe("false");
+  });
+  it("switch with string and inner assign (first)", () => {
+    const code = [
+      'switch "this"',
+      'case "this"',
+      "assign x = 5",
+      `case "that"`,
+      "assign x = 3",
+      "endswitch",
+      "return x",
+    ];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(1);
+    expect(modified[0].label).toBe("x");
+    expect(modified[0].value).toBe("5");
+  });
+  it("switch with string and inner assign (second)", () => {
+    const code = [
+      'switch "that"',
+      'case "this"',
+      "assign x = 5",
+      `case "that"`,
+      "assign x = 3",
+      "endswitch",
+      "return x",
+    ];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(1);
+    expect(modified[0].label).toBe("x");
+    expect(modified[0].value).toBe("3");
   });
 });
 
