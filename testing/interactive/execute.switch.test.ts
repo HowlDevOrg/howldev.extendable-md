661698bfe01f5df370b5a98c80c54c1b5c8636a6
Cody Howell <codyhowell44@gmail.com>
2026-06-05 22:28:22 -0600

    done with switch statements?

--- testing/interactive/execute.switch.test.ts
+++ testing/interactive/execute.switch.test.ts
@@ -31,6 +31,17 @@ describe("code can run switch statements with literals (1 level)", () => {
       "Did not find a return statement.",
     );
   });
+  it("switch with number and multiple inner lines", () => {
+    const code = ["switch 15", "case 15", "assign x = 5","assign y = 3","assign z = -2", "endswitch", "return x, y, z"];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(3);
+    expect(modified[0].label).toBe("x");
+    expect(modified[0].value).toBe("5");
+    expect(modified[1].label).toBe("y");
+    expect(modified[1].value).toBe("3");
+    expect(modified[2].label).toBe("z");
+    expect(modified[2].value).toBe("-2");
+  });
 });
 
 describe("code can run switch statements with literals (2 levels)", () => {
@@ -93,6 +104,44 @@ describe("code can run switch statements with literals (2 levels)", () => {
     expect(modified.length).toBe(1);
     expect(modified[0].label).toBe("x");
     expect(modified[0].value).toBe("3");
+  });
+  it("switch with string and multiple inner layers (first)", () => {
+    const code = [
+      'switch "this"',
+      'case "this"',
+      "assign x = 5",
+      "assign y = 3",
+      `case "that"`,
+      "assign x = 3",
+      "assign y = 5",
+      "endswitch",
+      "return x, y",
+    ];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(2);
+    expect(modified[0].label).toBe("x");
+    expect(modified[0].value).toBe("5");
+    expect(modified[1].label).toBe("y");
+    expect(modified[1].value).toBe("3");
+  });
+  it("switch with string and multiple inner layers (second)", () => {
+    const code = [
+      'switch "that"',
+      'case "this"',
+      "assign x = 5",
+      "assign y = 3",
+      `case "that"`,
+      "assign x = 3",
+      "assign y = 5",
+      "endswitch",
+      "return x, y",
+    ];
+    const modified = ExecuteCode(paramDefs, values, code);
+    expect(modified.length).toBe(2);
+    expect(modified[0].label).toBe("x");
+    expect(modified[0].value).toBe("3");
+    expect(modified[1].label).toBe("y");
+    expect(modified[1].value).toBe("5");
   });
 });
 
